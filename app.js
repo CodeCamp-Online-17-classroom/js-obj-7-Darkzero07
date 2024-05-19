759f1e8e892efc07989c18c36a29290553ae018d
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 22:46:54 +0700

    js lab 53

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// write code here
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+// let salaries = {};
+
+function isEmptyAndSum(obj) {
+  if (Object.keys(obj).length === 0) {
+    return 0;
+  } else {
+    let value = Object.values(obj);
+    let sum = value.reduce((acc, val) => {
+      return acc + val;
+    });
+    return sum
+  }
+}
+
+console.log(isEmptyAndSum(salaries));
+
+
